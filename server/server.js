de51e0bc9a38a57449d2b7314e4245252c697761
Oswaldo Franco <franco.oswaldo@gmail.com>
2018-07-11 13:32:58 -0500

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,55 @@
+require('./config/config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.get('/usuario', function(req, res) {
+    res.json('get usuario')
+})
+
+
+app.post('/usuario', function(req, res) {
+
+    let body = req.body
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: "El nombre es requerido"
+        })
+    } else {
+        res.json({
+            persona: body
+        })
+    }
+
+    res.json({
+        persona: body
+    })
+})
+
+
+app.put('/usuario/:id', function(req, res) {
+
+    let cod = req.params.id
+    res.json({
+        cod
+    })
+})
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete usuario')
+})
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log("escuchando puerto: ", process.env.PORT);
+})
